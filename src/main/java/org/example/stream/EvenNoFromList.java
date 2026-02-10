184c1ed3fcb9d5c0c12556471da5aae8178a2c5d
Ujwala Kabadi <mac@macs-MacBook-Pro.local>
2026-02-10 16:37:58 +0530

    added the even no from list in java 8

---
+++ src/main/java/org/example/stream/EvenNoFromList.java
@@ -0,0 +1,15 @@
+package org.example.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class EvenNoFromList {
+    public static void main(String[] args) {
+        List<Integer> lists = Arrays.asList(1,2,3,4,5,6,7);
+        List<Integer> listOfEvenNo = lists.stream().filter(list -> list % 2 == 0)
+                .collect(Collectors.toList());
+
+        listOfEvenNo.forEach(System.out::println);
+    }
+}
